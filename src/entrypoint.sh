76c2711a6792aacb95f7aaac6410dacff2c52df9
Andreas Kasper <djassi@web.de>
2021-12-21 01:37:14 +0100

    Added commands for spl-token

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 
-if [ -d "/wallet/" ]; then
-    if [ -f "/wallet/id.json" ]
-    fi
+#if [ -d "/wallet/" ]; then
+#    if [ -f "/wallet/id.json" ]; then
+#    fi
+#else
+#
+#fi
+
+if [ -f "/root/.config/solana/id.json" ]; then
+    echo ""
 else
-
+    echo "************************"
+    echo "please be sure to save your wallet example:"
+    echo "-v /safe/id.json:/root/.config/solana/id.json"
+    echo "************************"
 fi
-
-
-echo "************************"
-echo "please be sure to save your wallet example:"
-echo "-v /safe/id.json:/root/.config/solana/id.json"
-echo "************************"
 
 export PATH="/root/.local/share/solana/install/active_release/bin:$PATH" \
 
-if [ "$1" = 'bash' ]; then
-    bash
-else if [ "$1" = 'solana-keygen' ]; then
+case "$1" in
+    bash)
+        bash
+        ;;
+    solana-keygen | spl-token | solana)
         exec "$@"
-    else
+        ;;
+    spl-token)
+        exec "$@"
+        ;;
+    *)
         solana $@
-    fi
-fi
+        ;;
+esac
